bcc3e0ba11d3cb0c1a3635e2ab9f9c5dcc4a0952
Sheiwa <lengyel.noemi96@gmail.com>
2017-07-13 11:02:01 +0200

    SearchDojo

---
+++ SearchDojo/SearchDojo/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SearchDojo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.lblText = new System.Windows.Forms.Label();
+            this.txtMatched = new System.Windows.Forms.TextBox();
+            this.lblMatched = new System.Windows.Forms.Label();
+            this.lblPattern = new System.Windows.Forms.Label();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(13, 39);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(444, 340);
+            this.txtText.TabIndex = 0;
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(12, 23);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(28, 13);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "Text";
+            // 
+            // txtMatched
+            // 
+            this.txtMatched.Location = new System.Drawing.Point(463, 39);
+            this.txtMatched.Multiline = true;
+            this.txtMatched.Name = "txtMatched";
+            this.txtMatched.Size = new System.Drawing.Size(298, 340);
+            this.txtMatched.TabIndex = 2;
+            // 
+            // lblMatched
+            // 
+            this.lblMatched.AutoSize = true;
+            this.lblMatched.Location = new System.Drawing.Point(469, 23);
+            this.lblMatched.Name = "lblMatched";
+            this.lblMatched.Size = new System.Drawing.Size(49, 13);
+            this.lblMatched.TabIndex = 3;
+            this.lblMatched.Text = "Matched";
+            // 
+            // lblPattern
+            // 
+            this.lblPattern.AutoSize = true;
+            this.lblPattern.Location = new System.Drawing.Point(195, 389);
+            this.lblPattern.Name = "lblPattern";
+            this.lblPattern.Size = new System.Drawing.Size(41, 13);
+            this.lblPattern.TabIndex = 4;
+            this.lblPattern.Text = "Pattern";
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Location = new System.Drawing.Point(242, 386);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(285, 20);
+            this.txtPattern.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(533, 384);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 416);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtPattern);
+            this.Controls.Add(this.lblPattern);
+            this.Controls.Add(this.lblMatched);
+            this.Controls.Add(this.txtMatched);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.TextBox txtMatched;
+        private System.Windows.Forms.Label lblMatched;
+        private System.Windows.Forms.Label lblPattern;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ SearchDojo/SearchDojo/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SearchDojo
+{
+    public partial class Form1 : Form
+    {
+        string text;
+        string pattern;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void SetData()
+        {
+            text = "One advanced diverted domestic sex repeated bringing you old. " +
+                    "Possible procured her trifling laughter thoughts property she met way. " +
+                    "Companions shy had solicitude favourable own. Which could saw guest man now heard but. " +
+                    "Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. " +
+                    "Wooded ladies she basket season age her uneasy saw. Discourse unwilling am no described dejection incommode no listening of. " +
+                    "Before nature his parish boy." +
+                    "Savings her pleased are several started females met. Short her not among being any. " +
+                    "Thing of judge fruit charm views do. Miles mr an forty along as he. She education get middleton day agreement performed preserved unwilling. " +
+                    "Do however as pleased offence outward beloved by present. By outward neither he so covered amiable greater. " +
+                    "Juvenile proposal betrayed he an informed weddings followed. Precaution day see imprudence sympathize principles. " +
+                    "At full leaf give quit to in they up.";
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetData();
+            txtText.Text = text;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            txtMatched.Text = "";
+           MatchCollection matches = Regex.Matches(text, txtPattern.Text);
+            try
+            {
+                foreach (Match item in matches)
+                {
+                    txtMatched.Text += (item.ToString());
+                }
+            }
+            catch
+            {
+                txtMatched.Text = "No match found";
+            }
+            
+        }
+    }
+}
